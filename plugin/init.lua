0f7a93b4135449b8dc5022f65e9fae927aa36865
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2025-01-04 18:51:48 +0300

    use real suggestion

--- plugin/init.lua
+++ plugin/init.lua
@@ -2,11 +2,11 @@ local rktmb_deepseek_complete = require("rktmb-deepseek-complete")
 rktmb_deepseek_complete.log("Entered init.lua")
 
 _G.completion_handler = nil
+_G.current_extmark = nil -- Store the extmark ID
 
 vim.api.nvim_create_autocmd("InsertEnter", {
     pattern = "*",
     callback = function()
-        -- Set the completion handler
         _G.completion_handler = function()
             local current_line = vim.api.nvim_get_current_line()
             local current_col = vim.api.nvim_win_get_cursor(0)[2]
@@ -14,11 +14,23 @@ vim.api.nvim_create_autocmd("InsertEnter", {
 
             local suggestion = rktmb_deepseek_complete.generate_sentence()
 
-            vim.fn.complete(current_col, {
-                { word = suggestion, kind = "random sentence", menu = "[random]", icase = 1, abbr = current_word }
-            })
-        end
+            -- Clear previous suggestion if any
+            if _G.current_extmark then
+                vim.api.nvim_buf_del_extmark(0, _G.current_extmark.ns, _G.current_extmark.id)
+                _G.current_extmark = nil
+            end
 
+
+            local ns_id = vim.api.nvim_create_namespace("rktmb-deepseek-complete-ns")
+            local extmark_id = vim.api.nvim_buf_set_extmark(0, ns_id, vim.api.nvim_win_get_cursor(0)[1] - 1, current_col, {
+                virt_text = {{suggestion, "in-completion-item"}},
+                virt_text_pos = "overlay",
+                hl_mode = "combine" -- Important for proper highlighting
+            })
+
+            _G.current_extmark = {ns = ns_id, id = extmark_id}
+
+        end
 
         vim.keymap.set("i", "<M-PageDown>", function()
             vim.defer_fn(_G.completion_handler, 0)
@@ -31,6 +43,13 @@ vim.api.nvim_create_autocmd("InsertLeave", {
     pattern = "*",
     callback = function()
         vim.keymap.del("i", "<M-PageDown>")
-        _G.completion_handler = nil -- Clear the handler when leaving insert mode
+        _G.completion_handler = nil
+
+        -- Clear the suggestion on InsertLeave
+        if _G.current_extmark then
+            vim.api.nvim_buf_del_extmark(0, _G.current_extmark.ns, _G.current_extmark.id)
+            _G.current_extmark = nil
+        end
     end
 })
+
