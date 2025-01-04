00d87b20d6de21226f6ff40cd8f9739980909bbd
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2025-01-04 18:26:01 +0300

    fix not allowed

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,10 +1,14 @@
 local rktmb_deepseek_complete = require("rktmb-deepseek-complete")
 rktmb_deepseek_complete.log("Entered init.lua")
 
+-- Use a global variable to store the completion handler
+_G.completion_handler = nil
+
 vim.api.nvim_create_autocmd("InsertEnter", {
     pattern = "*",
     callback = function()
-        vim.keymap.set("i", "<M-PageDown>", function()
+        -- Set the completion handler
+        _G.completion_handler = function()
             local current_line = vim.api.nvim_get_current_line()
             local current_col = vim.api.nvim_win_get_cursor(0)[2]
             local current_word = vim.fn.expand("<cword>")
@@ -14,6 +18,13 @@ vim.api.nvim_create_autocmd("InsertEnter", {
             vim.fn.complete(current_col, {
                 { word = suggestion, kind = "random sentence", menu = "[random]", icase = 1, abbr = current_word }
             })
+        end
+
+
+        vim.keymap.set("i", "<M-PageDown>", function()
+            -- Call the completion handler with a timer to defer execution
+            vim.defer_fn(_G.completion_handler, 0)
+            return "" -- Return empty string to avoid inserting characters.
         end, { noremap = true, expr = true, silent = true })
     end
 })
@@ -22,6 +33,6 @@ vim.api.nvim_create_autocmd("InsertLeave", {
     pattern = "*",
     callback = function()
         vim.keymap.del("i", "<M-PageDown>")
+        _G.completion_handler = nil -- Clear the handler when leaving insert mode
     end
 })
-
