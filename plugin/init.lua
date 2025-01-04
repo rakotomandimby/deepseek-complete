6549825afdb9c94b6583af4b6cf63761bb3f0e8b
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2025-01-05 00:41:14 +0300

    clear when quitting the mode

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,5 +1,7 @@
 local rktmb_deepseek_complete = require("rktmb-deepseek-complete")
 
+_G.ns_id = vim.api.nvim_create_namespace('rktmb-deepseek-complete')
+
 _G.suggest_random_sentence = function()  -- Make this function global
   local current_line = vim.api.nvim_get_current_line()
   local current_row = vim.api.nvim_win_get_cursor(0)[1]
@@ -7,12 +9,12 @@ _G.suggest_random_sentence = function()  -- Make this function global
   vim.api.nvim_win_set_cursor(0, { current_row, #current_line })
 
   local sentence = rktmb_deepseek_complete.generate_sentence()
+  local lines = vim.split(sentence, "\n", true)
 
   local opts = {
     virt_text = { { sentence, "Comment" } },  -- Use "Comment" highlight group
     virt_text_pos = "overlay",
   }
-  local ns_id = vim.api.nvim_create_namespace('rktmb-deepseek-complete')
   vim.api.nvim_buf_set_extmark(0, ns_id, current_row - 1, #current_line, opts)
 
   local augroup_id = vim.api.nvim_create_augroup("RktmbDeepseekCompleteSuggestions", { clear = true })
@@ -26,6 +28,13 @@ _G.suggest_random_sentence = function()  -- Make this function global
   })
 end
 
+-- when quitting insert mode, clear the suggestions
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
+  end,
+})
 
 vim.api.nvim_set_keymap("i", "<M-PageDown>", "<Cmd>lua suggest_random_sentence()<CR>", { noremap = true, silent = true })
 
