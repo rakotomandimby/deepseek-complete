f8da52ca6407e1e1d1938b4cca6e10e425370f78
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2025-01-04 23:25:35 +0300

    IDK

--- plugin/init.lua
+++ plugin/init.lua
@@ -24,34 +24,30 @@ local function show_suggestion()
   -- Get the current buffer and cursor position *BEFORE* moving the cursor
   local bufnr = vim.api.nvim_get_current_buf()
 
-  -- Use a callback to ensure the cursor position is retrieved *after* the keys are processed
-  vim.api.nvim_feedkeys(
-    vim.api.nvim_replace_termcodes("<Esc>A", true, false, true),
-    'n',
-    false,
-    function()
-      local current_cursor_pos = vim.api.nvim_win_get_cursor(0) -- Get cursor pos after moving
+  -- Move the cursor to the end of the line
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>A", true, false, true), 'n', false)
 
-      -- Generate a random sentence
-      local suggestion = rktmb_deepseek_complete.generate_sentence()
+  -- Get the current cursor position after moving
+  local current_cursor_pos = vim.api.nvim_win_get_cursor(0)
 
-      -- Create a namespace for our extmarks
-      local ns_id = vim.api.nvim_create_namespace('rktmb-deepseek-complete')
+  -- Generate a random sentence
+  local suggestion = rktmb_deepseek_complete.generate_sentence()
 
-      _G.current_extmarks = {}
+  -- Create a namespace for our extmarks
+  local ns_id = vim.api.nvim_create_namespace('rktmb-deepseek-complete')
 
-      -- Set the extmark *at the current cursor position*
-      local extmark_id = vim.api.nvim_buf_set_extmark(bufnr, ns_id, current_cursor_pos[1] - 1, current_cursor_pos[2], {
-        virt_text = { { suggestion, "InlineSuggestion" } },
-        virt_text_pos = 'overlay',
-      })
-      table.insert(_G.current_extmarks, { ns = ns_id, id = extmark_id })
+  _G.current_extmarks = {}
 
-      -- Return to insert mode
-      vim.api.nvim_feedkeys("a", 'n', false)
-    end)
+  -- Set the extmark *at the current cursor position*
+  local extmark_id = vim.api.nvim_buf_set_extmark(bufnr, ns_id, current_cursor_pos[1] - 1, current_cursor_pos[2], {
+    virt_text = { { suggestion, "InlineSuggestion" } },
+    virt_text_pos = 'overlay',
+  })
+  table.insert(_G.current_extmarks, { ns = ns_id, id = extmark_id })
+
+  -- Return to insert mode
+  vim.api.nvim_feedkeys("a", 'n', false)
 end
-
 
 -- Map <M-PageDown> to show_suggestion in insert mode
 vim.keymap.set('i', '<M-PageDown>', show_suggestion, { noremap = true, silent = true })
@@ -71,4 +67,3 @@ vim.api.nvim_create_autocmd("InsertLeave", {
     clear_suggestion()
   end,
 })
-
