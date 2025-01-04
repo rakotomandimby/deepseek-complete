20a5d111bf608f100b0c065fcd7687b65c34def8
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2025-01-05 01:19:23 +0300

    use another model

--- plugin/init.lua
+++ plugin/init.lua
@@ -3,47 +3,43 @@ local rktmb_deepseek_complete = require("rktmb-deepseek-complete")
 _G.ns_id = vim.api.nvim_create_namespace('rktmb-deepseek-complete')
 
 _G.suggest_random_sentence = function()
-  -- 1. Move cursor to the end of the line *FIRST*  (This is correct and important)
   local current_row = vim.api.nvim_win_get_cursor(0)[1]
   local current_line = vim.api.nvim_get_current_line()
-  vim.api.nvim_win_set_cursor(0, { current_row, #current_line + 1}) -- +1 to move past end of line
 
+  -- Move to the end of the current line *before* generating the sentence
+  vim.api.nvim_win_set_cursor(0, {current_row, #current_line +1})
 
   local sentence = rktmb_deepseek_complete.generate_sentence()
   local lines = vim.split(sentence, "\n", true)
 
-  -- Log the lines (for debugging)
-  rktmb_deepseek_complete.log("Lines:")
-  for _, line in ipairs(lines) do
-    rktmb_deepseek_complete.log(line)
-  end
-  rktmb_deepseek_complete.log("")
 
-  -- Use virtual text for multi-line suggestions.  Crucially, use \n as the actual newline
+  -- Construct virtual text chunks with proper newlines
   local chunks = {}
-  for i, line in ipairs(lines) do
-    table.insert(chunks, { line, "Comment" })  -- Use "Comment" highlight group for grey
+  for _, line in ipairs(lines) do
+    table.insert(chunks, {line, "Comment"})  -- "Comment" highlight for grey
 
-    -- Add a newline chunk *after* each line except the last
-    if i < #lines then
-      table.insert(chunks, { "\n", "Comment" }) -- Newline with "Comment" highlighting
+    -- Add newline chunk after each line *except* the last
+    if _ < #lines then
+      table.insert(chunks, {"\n", "Comment"}) -- Actual newline character
     end
   end
 
+  local current_col = vim.api.nvim_win_get_cursor(0)[2]
 
-  local current_col = vim.api.nvim_win_get_cursor(0)[2] -- Get updated column
+  vim.api.nvim_buf_set_extmark(0, ns_id, current_row - 1, current_col, {
+    virt_text = chunks,
+    virt_text_pos = 'overlay'
+  })
 
-  vim.api.nvim_buf_set_extmark(0, ns_id, current_row - 1, current_col, { virt_text = chunks, virt_text_pos = 'overlay' })
-
-  -- Clear the suggestion on TextChangedI (unchanged)
-  local augroup_id = vim.api.nvim_create_augroup("RktmbDeepseekCompleteSuggestions", { clear = true })
+  -- Clear on TextChangedI (this part is correct)
+  local augroup_id = vim.api.nvim_create_augroup("RktmbDeepseekCompleteSuggestions", {clear = true})
   vim.api.nvim_create_autocmd("TextChangedI", {
     group = augroup_id,
     buffer = 0,
     callback = function()
       vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
       vim.api.nvim_del_augroup_by_id(augroup_id)
-    end,
+    end
   })
 end
 
@@ -52,7 +48,7 @@ vim.api.nvim_create_autocmd("InsertLeave", {
   pattern = "*",
   callback = function()
     vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
-  end,
+  end
 })
 
-vim.api.nvim_set_keymap("i", "<M-PageDown>", "<Cmd>lua suggest_random_sentence()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<M-PageDown>", "<Cmd>lua suggest_random_sentence()<CR>", {noremap = true, silent = true})
