baf02823e08a6b1a12928c0d7fbdf1348b791199
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2025-01-05 00:25:23 +0300

    call it global

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,30 +1,26 @@
 local rktmb_deepseek_complete = require("rktmb-deepseek-complete")
 
-local function suggest_random_sentence()
+_G.suggest_random_sentence = function()  -- Make this function global
   local current_line = vim.api.nvim_get_current_line()
   local current_row = vim.api.nvim_win_get_cursor(0)[1]
 
-  -- Move cursor to the end of the line
   vim.api.nvim_win_set_cursor(0, { current_row, #current_line })
 
   local sentence = rktmb_deepseek_complete.generate_sentence()
 
-  -- Create a virtual text for the suggestion
   local opts = {
-    virt_text = { { sentence, "Comment" } },
-    virt_text_pos = "overlay", -- Important for pushing lines down
+    virt_text = { { sentence, "Comment" } },  -- Use "Comment" highlight group
+    virt_text_pos = "overlay",
   }
-  vim.api.nvim_buf_set_extmark(0, vim.fn.nsID("rktmb-deepseek-complete"), current_row -1, #current_line, opts)
+  vim.api.nvim_buf_set_extmark(0, vim.fn.nsID("rktmb-deepseek-complete"), current_row - 1, #current_line, opts)
 
-
-  -- Autocmd to clear the suggestion when typing
   local augroup_id = vim.api.nvim_create_augroup("RktmbDeepseekCompleteSuggestions", { clear = true })
   vim.api.nvim_create_autocmd("TextChangedI", {
     group = augroup_id,
     buffer = 0,
     callback = function()
       vim.api.nvim_buf_clear_namespace(0, vim.fn.nsID("rktmb-deepseek-complete"), 0, -1)
-      vim.api.nvim_del_augroup_by_id(augroup_id)
+      vim.api.nvim_del_augroup_by_id(augroup_id) -- Clean up the autocommand group
     end,
   })
 end
