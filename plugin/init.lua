608dba02feaff09bf19f47847e06370a2b946eaf
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2025-01-05 00:04:02 +0300

    use Gemini proposition

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,84 +1,34 @@
--- Require the module containing the generate_sentence function
-local deepseek_complete = require('rktmb-deepseek-complete')
+local rktmb_deepseek_complete = require("rktmb-deepseek-complete")
 
--- Create a table to store our functions and state
-local M = {}
+local function suggest_random_sentence()
+  local current_line = vim.api.nvim_get_current_line()
+  local current_row = vim.api.nvim_win_get_cursor(0)[1]
 
--- Function to clear the suggestion when the user types or moves
-function M.clear_suggestion()
-  if M._suggestion then
-    local bufnr = M._suggestion.bufnr
-    local start_line = M._suggestion.start_line
-    local end_line = M._suggestion.end_line
-    local namespace = M._suggestion.namespace
+  -- Move cursor to the end of the line
+  vim.api.nvim_win_set_cursor(0, { current_row, #current_line })
 
-    -- Delete the inserted lines
-    vim.api.nvim_buf_set_lines(bufnr, start_line, end_line, false, {})
+  local sentence = rktmb_deepseek_complete.generate_sentence()
 
-    -- Clear the namespace (highlights)
-    vim.api.nvim_buf_clear_namespace(bufnr, namespace, start_line, end_line)
-
-    -- Clear the stored suggestion
-    M._suggestion = nil
-
-    -- Clear the autocmd group
-    vim.cmd('augroup SuggestionAutocmd | autocmd! | augroup END')
-  end
-end
-
--- Function to show the suggestion when triggered
-function M.show_suggestion()
-  -- Clear any existing suggestion
-  M.clear_suggestion()
-
-  -- Move cursor to end of line
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>A", true, false, true), 'n', false)
-
-  -- Generate a random multiline sentence
-  local suggestion = deepseek_complete.generate_sentence()
-
-  -- Get current buffer and cursor position
-  local bufnr = vim.api.nvim_get_current_buf()
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  local line = cursor[1] - 1  -- Zero-based index
-
-  -- Split the suggestion into lines
-  local lines = vim.split(suggestion, '\n', true)
-
-  -- Insert the suggestion lines after the current line
-  vim.api.nvim_buf_set_lines(bufnr, line + 1, line + 1, false, lines)
-
-  -- Apply grey color (#808080) to the inserted text
-  local namespace = vim.api.nvim_create_namespace('suggestion_ns')
-  local hl_group = 'SuggestionGrey'
-
-  -- Define the highlight group for grey color
-  vim.cmd('highlight ' .. hl_group .. ' guifg=#808080')
-
-  -- Apply the highlight to each line of the suggestion
-  for i = 1, #lines do
-    vim.api.nvim_buf_add_highlight(bufnr, namespace, hl_group, line + i, 0, -1)
-  end
-
-  -- Create an autocmd group to clear the suggestion when typing or moving
-  vim.cmd([[
-    augroup SuggestionAutocmd
-    autocmd!
-    autocmd TextChangedI,CursorMovedI * lua require('plugin.init').clear_suggestion()
-    augroup END
-    ]])
-
-  -- Store the suggestion state to clear it later
-  M._suggestion = {
-    bufnr = bufnr,
-    start_line = line + 1,
-    end_line = line + 1 + #lines,
-    namespace = namespace,
+  -- Create a virtual text for the suggestion
+  local opts = {
+    virt_text = { { sentence, "Comment" } },
+    virt_text_pos = "overlay", -- Important for pushing lines down
   }
+  vim.api.nvim_buf_set_extmark(0, vim.fn.nsID("rktmb-deepseek-complete"), current_row -1, #current_line, opts)
+
+
+  -- Autocmd to clear the suggestion when typing
+  local augroup_id = vim.api.nvim_create_augroup("RktmbDeepseekCompleteSuggestions", { clear = true })
+  vim.api.nvim_create_autocmd("TextChangedI", {
+    group = augroup_id,
+    buffer = 0,
+    callback = function()
+      vim.api.nvim_buf_clear_namespace(0, vim.fn.nsID("rktmb-deepseek-complete"), 0, -1)
+      vim.api.nvim_del_augroup_by_id(augroup_id)
+    end,
+  })
 end
 
--- Map <M-PageDown> in INSERT mode to trigger the suggestion
-vim.api.nvim_set_keymap('i', '<M-PageDown>', '<Cmd>lua require("plugin.init").show_suggestion()<CR>', { noremap = true, silent = true })
 
-return M
+vim.api.nvim_set_keymap("i", "<M-PageDown>", "<Cmd>lua suggest_random_sentence()<CR>", { noremap = true, silent = true })
 
