2902b2813b566d5f3da5c548b5f93418404724d3
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2025-01-06 22:32:16 +0300

    logs

--- plugin/init.lua
+++ plugin/init.lua
@@ -7,6 +7,7 @@ _G.current_extmark_id = nil
 _G.current_suggestion = nil
 
 local function process_deepseek_response(response)
+  rktmb_deepseek_complete.log("DeepSeek API response handler called")
   if response.status == 200 then
     -- Use vim.schedule_wrap to defer the json_decode call
     vim.schedule_wrap(function()
