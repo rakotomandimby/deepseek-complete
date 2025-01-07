04a95c85bbd0d040d38420ceff0354b09730305a
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2025-01-07 19:43:43 +0300

    place it better

--- plugin/init.lua
+++ plugin/init.lua
@@ -16,23 +16,6 @@ local default_opts = {
 
 -- Read user configuration
 local user_opts = vim.tbl_deep_extend("force", default_opts, vim.g.rktmb_deepseek_complete_opts or {})
-local deepseek_request_body = {
-  model = "deepseek-chat",
-  echo = false,
-  frequency_penalty = 0,
-  max_tokens = 4096,
-  presence_penalty = 0,
-  stop = nil,
-  stream = false,
-  stream_options = nil,
-  temperature = 1,
-  top_p = 1,
-  messages = rktmb_deepseek_complete.build_messages_table(
-    rktmb_deepseek_complete.text_before_cursor,
-    rktmb_deepseek_complete.text_after_cursor
-  )
-}
-
 local function process_deepseek_response(response)
   local response_body = vim.fn.json_decode(response.body)
   if response_body.choices and #response_body.choices > 0 then
@@ -44,6 +27,24 @@ local function process_deepseek_response(response)
 end
 
 _G.suggest = function()
+
+  local deepseek_request_body = {
+    model = "deepseek-chat",
+    echo = false,
+    frequency_penalty = 0,
+    max_tokens = 4096,
+    presence_penalty = 0,
+    stop = nil,
+    stream = false,
+    stream_options = nil,
+    temperature = 1,
+    top_p = 1,
+    messages = rktmb_deepseek_complete.build_messages_table(
+      rktmb_deepseek_complete.text_before_cursor,
+      rktmb_deepseek_complete.text_after_cursor
+    )
+  }
+
   -- Asynchronously make the POST request
   curl.post('https://api.deepseek.com/chat/completions', {
     body = vim.fn.json_encode(deepseek_request_body),
